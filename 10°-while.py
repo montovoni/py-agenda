90d3bb97407476f171c63cd7da6f8fe3eb7a4251
DouglasMontovoni <57241037+DouglasMontovoni@users.noreply.github.com>
2021-11-13 02:59:19 -0300

    Automação-Agenda-Python

---
+++ 10°-while.py
@@ -0,0 +1,7 @@
+contador = 0
+
+while contador <10:
+    print(contador," - Essa condição é verdadeira")
+    contador += 1
+
+print("\nPrograma encerrado!")
---
+++ 8°-tuplas.py
@@ -0,0 +1,4 @@
+cores = ("roxo" , "azul", "vermelho" , "amarelo") #TUPLAS SÂO IMUTÀVEIS
+
+for cor in cores:
+    print(cor)
